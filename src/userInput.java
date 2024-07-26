3d3372f7c330a7b52d3367eaa34a686864cd8879
Naymur Rahman <nrtusher1@gmail.com>
2024-07-26 16:16:19 +0600

    Assignment 3 done

--- src/userInput.java
+++ src/userInput.java
@@ -1,19 +1,51 @@
+//import java.util.Scanner;
+//
+//public class userInput {
+//    public static void main(String[] args) {
+//        int number;
+//        String name;
+//        Scanner input = new Scanner(System.in);
+//        System.out.print("Enter any number : ");
+//        number = input.nextInt();
+//
+//        input.nextLine();
+//        System.out.print("Enter your name : ");
+//        name = input.nextLine();
+//
+//    //! The next method doesn't work when there is a gap in the string, so we have to use the nextLine method to solve the problem
+//        System.out.println("Number :" + number);
+//        System.out.println("Your name :" + name);
+//    }
+//}
+
+
+/*
+! Assignment -3 :
+*/
+
 import java.util.Scanner;
 
 public class userInput {
     public static void main(String[] args) {
-        int number;
-        String name;
+        int id;
+        float price;
+        String title, description, category;
+// Initializing the Scanner method
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter any number : ");
-        number = input.nextInt();
 
+        System.out.print("Enter the title: ");
+        title = input.nextLine();
+        System.out.print("Enter the ID: ");
+        id = input.nextInt();
+        System.out.print("Enter the price: ");
+        price = input.nextFloat();
         input.nextLine();
-        System.out.print("Enter your name : ");
-        name = input.nextLine();
+        System.out.print("Enter the description: ");
+        description = input.nextLine();
+        System.out.print("Enter the category: ");
+        category = input.nextLine();
 
-    //! The next method doesnt work when there is a gap in the string, so we have to use the nextLine method to solve the problem
-        System.out.println("Number :" + number);
-        System.out.println("Your name :" + name);
+        System.out.print("Print out from the Product Function: " + "\nPhone Id :" + id + "\nPhone title: " + title + "\nPhone price:" + price + "\nPhones description :" + description + "\nPhones Category : " + category + "\n");
+
     }
 }
