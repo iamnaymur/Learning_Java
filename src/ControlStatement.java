f0d890823b251b6fee2236f64901e2a2cfdd8d88
Naymur Rahman <nrtusher1@gmail.com>
2024-07-27 23:58:48 +0600

    Changes to check

--- src/ControlStatement.java
+++ src/ControlStatement.java
@@ -12,9 +12,9 @@ public class ControlStatement {
 
     public static void checkVoter(int age) {
         if (age >= 18) {
-            System.out.print("Valid voter.");
+            System.out.print("This user is a Valid voter.");
         } else {
-            System.out.print("Not a valid voter.");
+            System.out.print("This user is Not a valid voter.");
         }
     }
 }
