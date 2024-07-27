2ccd46cc20bebe19563ca3392744d07914e28105
Naymur Rahman <nrtusher1@gmail.com>
2024-07-27 20:20:40 +0600

    assignment 5

---
+++ src/ControlStatement.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class ControlStatement {
+    static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int age;
+        System.out.print("Put your age :");
+        age = input.nextInt();
+        checkVoter(age);
+    }
+
+    public static void checkVoter(int age) {
+
+        if (age >= 18) {
+            System.out.print("Valid voter.");
+        } else {
+            System.out.print("Not a valid voter.");
+        }
+    }
+}
