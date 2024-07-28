5f948878a6d8c178e652a13e4354a05a8c4a41b2
Naymur Rahman <nrtusher1@gmail.com>
2024-07-28 11:37:29 +0600

    video 27

--- src/ControlStatement.java
+++ src/ControlStatement.java
@@ -8,13 +8,30 @@ public class ControlStatement {
         System.out.print("Put your age :");
         age = input.nextInt();
         checkVoter(age);
+        checkLetter();
     }
 
+//    Check voter function
     public static void checkVoter(int age) {
         if (age >= 18) {
             System.out.print("This user is a Valid voter.");
         } else {
             System.out.print("This user is Not a valid voter.");
+        }
+    }
+
+//    Check letter function
+    public static void checkLetter() {
+        System.out.print("Please enter your letter : ");
+        char ch = input.next().charAt(0);
+        if (ch >= 'A' && ch <= 'Z') {
+            System.out.print("This is a capital letter.");
+        }
+        else if(ch >= 'a' && ch <= 'z'){
+            System.out.print("This is not a capital letter.");
+        }
+        else {
+            System.out.print("This is not a letter.");
         }
     }
 }
