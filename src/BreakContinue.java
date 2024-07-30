b9a1750d1ded0bf8359d09496b023f7f3a289b4b
Naymur Rahman <nrtusher1@gmail.com>
2024-07-30 20:06:25 +0600

    Done till video-50.

--- src/BreakContinue.java
+++ src/BreakContinue.java
@@ -1,13 +1,13 @@
 public class BreakContinue {
     public static void main(String[] args) {
-        for (int i = 0; i <= 10; i++) {
-            if (i == 5) {
+        for (int number= 0; number<= 10; number++) {
+            if (number== 5) {
                 break; //! It stops the loop completely and takes you out of the loop.
-            } else if (i == 3) {
+            } else if (number== 3) {
                 continue; //! It skips the rest of the current loop iteration and move to the next iteration.
             }
 
-            System.out.println(i);
+            System.out.println(number);
         }
     }
 }
---
+++ src/LoopPrograms.java
@@ -0,0 +1,44 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class LoopPrograms {
+    static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+//        PrintNumbers();
+        SquareSeries();
+    }
+
+
+    //    Print sum of all the even numbers from m to n.
+    public static void PrintNumbers() {
+        int start, last, sum = 0;
+
+        System.out.print("Enter the starting number :");
+        start = input.nextInt();
+        System.out.print("Enter the end number :");
+        last = input.nextInt();
+
+
+        for (int i = start; i <= last; i++) {
+            if (i % 2 != 0) {
+                sum = sum + i;
+                System.out.println(i);
+            }
+        }
+        System.out.println("total =" + sum);
+    }
+
+    public static void SquareSeries() {
+        int n, sum =0;
+        System.out.print("Enter the last number :");
+        n = input.nextInt();
+
+        for (int i =1; i<=n; i++ ){
+            System.out.println(i+"x"+i);
+            sum = sum + i*i;
+        }
+        System.out.print("Total sum of the series :"+sum);
+    }
+}
+
