d565f1feb79aa46bd2cb5827ea2507148733b1ca
Naymur Rahman <nrtusher1@gmail.com>
2024-07-31 11:48:06 +0600

    video 54

---
+++ src/Series/PrimeNumber.java
@@ -0,0 +1,30 @@
+package Series;
+
+import java.util.Scanner;
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter any positive number: ");
+        int num = input.nextInt();
+        if (isPrime(num)){
+            System.out.println("Prime Number");
+        }
+        else {
+            System.out.println("Not Prime Number");
+        }
+    }
+
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i < num/2; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
