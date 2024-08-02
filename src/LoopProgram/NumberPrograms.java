1de4d61cdcea33f0640e0b7aa26980bc7db8bbe9
Naymur Rahman <nrtusher1@gmail.com>
2024-08-02 17:46:52 +0600

    assignment 14 - palindrome number

--- src/LoopProgram/NumberPrograms.java
+++ src/LoopProgram/NumberPrograms.java
@@ -7,7 +7,8 @@ public class NumberPrograms {
 
     public static void main(String[] args) {
 //        sumOfDigits();
-        reverseNumber();
+//        reverseNumber();
+        findPalindrome();
     }
 
     //!    Sum of digits
@@ -37,6 +38,25 @@ public class NumberPrograms {
             sum = sum * 10 + remainder;
         }
         System.out.print("The reverse number is: " + sum);
+    }
+
+    public static void findPalindrome() {
+        int startNum, endNum, temp, remainder, sum;
+        System.out.println("Enter the starting number: ");
+        startNum = input.nextInt();
+        System.out.println("Enter the ending number: ");
+        endNum = input.nextInt();
+
+        for (int i = startNum; i <= endNum; i++) {
+            sum = 0;
+            for (temp = i; temp != 0; temp = temp / 10) {
+                remainder = temp % 10;
+                sum = sum * 10 + remainder;
+            }
+            if (sum == i) {
+                System.out.println(i);
+            }
+        }
     }
 
 }
