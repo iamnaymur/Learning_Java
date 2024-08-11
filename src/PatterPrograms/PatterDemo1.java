087c204486cbda0c368c7cac8c6394e8ccfbfc01
Naymur Rahman <nrtusher1@gmail.com>
2024-08-11 18:04:47 +0600

    assignment 17

--- src/PatterPrograms/PatterDemo1.java
+++ src/PatterPrograms/PatterDemo1.java
@@ -3,14 +3,26 @@ package PatterPrograms;
 import java.util.Scanner;
 
 public class PatterDemo1 {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter the number : ");
-        int num = input.nextInt();
 
-        for (int row = num; row >= 1; row--) {
+    public static void main(String[] args) {
+//        Scanner input = new Scanner(System.in);
+//        System.out.print("Enter the number : ");
+//        int num = input.nextInt();
+//
+//        for (int row = num; row >= 1; row--) {
+//            for (int col = 1; col <= row; col++) {
+//                System.out.print(" " + col);
+//            }
+//            System.out.println();
+//        }
+        assignmentPatter();
+    }
+
+
+    public static void assignmentPatter() {
+        for (int row = 1; row <= 3; row++) {
             for (int col = 1; col <= row; col++) {
-                System.out.print(" " + col);
+                System.out.print(" " + col % 2);
             }
             System.out.println();
         }
