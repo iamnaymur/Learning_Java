db4a65c6f01e2f07a55c47031c68594d92ed0ed6
Naymur Rahman <nrtusher1@gmail.com>
2024-08-15 21:57:09 +0600

    Matrix Part-3 | Diagonal Matrix

--- src/array/ArrayMatrix.java
+++ src/array/ArrayMatrix.java
@@ -6,7 +6,8 @@ public class ArrayMatrix {
     static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
-        matrixPart1();
+//        matrixPart1();
+        diagonalMatrix();
     }
 
     public static void matrixPart1() {
@@ -16,7 +17,9 @@ public class ArrayMatrix {
         System.out.print("Enter elements for A matrix :");
         //Getting input for matrix A
         for (int row = 0; row < A.length; row++) {
+
             for (int col = 0; col < A[row].length; col++) {
+
                 System.out.print("A[" + row + "][" + col + "] = ");
                 A[row][col] = input.nextInt();
             }
@@ -38,6 +41,7 @@ public class ArrayMatrix {
             }
             System.out.println();
         }
+
         System.out.println("Matrix B:");
         for (int row = 0; row < B.length; row++) {
             for (int col = 0; col < B[row].length; col++) {
@@ -45,5 +49,44 @@ public class ArrayMatrix {
             }
             System.out.println();
         }
+
+        //Adding two matrix
+        for (int row = 0; row < A.length; row++) {
+            for (int col = 0; col < A[row].length; col++) {
+                System.out.print(" " + A[row][col] + B[row][col]);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void diagonalMatrix() {
+        int[][] A = new int[3][3];
+        int sumOfDiagonalElements = 0, sumOfUpperElements = 0, sumOfLowerElements = 0;
+
+        for (int row = 0; row < A.length; row++) {
+            for (int col = 0; col < A[row].length; col++) {
+                A[row][col] = input.nextInt();
+            }
+        }
+
+        for (int row = 0; row < A.length; row++) {
+            for (int col = 0; col < A[row].length; col++) {
+                if (row == col) {
+                    sumOfDiagonalElements = sumOfDiagonalElements + A[row][col];
+
+                }
+                if (row > col) {
+                    sumOfLowerElements = sumOfLowerElements + A[row][col];
+                }
+                if (row < col) {
+                    sumOfUpperElements = sumOfUpperElements + A[row][col];
+                }
+            }
+        }
+
+        System.out.print("Diagonal sum of elements :" + sumOfDiagonalElements);
+        System.out.print("Lower elements sum :" + sumOfLowerElements);
+        System.out.print("Upper elements sum :" + sumOfUpperElements);
+
     }
 }
