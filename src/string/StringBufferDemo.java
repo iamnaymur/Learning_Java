1d155fd5e03476efdf58f0663c258ac7a2f6b38a
Naymur Rahman <nrtusher1@gmail.com>
2024-08-30 18:38:58 +0600

    String Palindrome

---
+++ src/string/StringBufferDemo.java
@@ -0,0 +1,36 @@
+package string;
+
+import java.util.Scanner;
+
+public class StringBufferDemo {
+    public static void main(String[] args) {
+//        stringBufferDemo1();
+        palindrome();
+    }
+
+    public static void stringBufferDemo1() {
+        StringBuffer sb = new StringBuffer("Kire");
+        sb.append("Hello World");
+
+        System.out.println(sb);
+    }
+
+    public static void palindrome() {
+        Scanner input = new Scanner(System.in);
+        String userString;
+        userString = input.nextLine();
+
+        StringBuffer checkPalindrome = new StringBuffer(userString);
+
+        checkPalindrome.reverse();
+
+//        if (reversed.equals(userString)) {
+//            System.out.println("Palindrome");
+//        } else {
+//            System.out.println("Not a palindrome");
+//        }
+        if (checkPalindrome.toString().equals(userString)) {
+            System.out.println("Palindrome");
+        }
+    }
+}
