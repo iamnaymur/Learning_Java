d5f488b725d8354723dfe64576c56dd5c93f7fb9
Naymur Rahman <nrtusher1@gmail.com>
2024-09-16 17:36:56 +0600

    done till video 100

--- src/wrapperClass/NumberConversion.java
+++ src/wrapperClass/NumberConversion.java
@@ -1,5 +1,12 @@
 package wrapperClass;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.Random;
+
 public class NumberConversion {
     public static void main(String[] args) {
         String binary = "11000110";
@@ -13,6 +20,31 @@ public class NumberConversion {
         String hexaDecimal = "A";
         Integer decimal3 = Integer.parseInt(hexaDecimal, 16);
         System.out.println(decimal3);
+
+
+        int decimal4 = 15;
+        String output = Integer.toHexString(decimal4);
+        System.out.println(output);
+
+        //date class in java
+        Date date = new Date();
+        DateFormat format = new SimpleDateFormat("MM/dd/yyyy");
+        String dateString = format.format(date);
+        System.out.println(dateString);
+
+        // java time class
+
+        LocalTime time = LocalTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("hh:mm:ss");
+
+        String formattedTime = time.format(formatter);
+        System.out.println(formattedTime);
+
+
+        Random rand = new Random();
+        int randomNum = rand.nextInt(100); //random number from 0 to 99
+        System.out.println(randomNum);
+
     }
 
 }
