75378d79bde6992b9318441ad10c0c5e5529dfd6
Naymur Rahman <nrtusher1@gmail.com>
2024-09-19 19:16:34 +0600

    difference method and constructor

--- src/oop/Teacher.java
+++ src/oop/Teacher.java
@@ -2,18 +2,38 @@ package oop;
 
 public class Teacher {
     String name, gender;
-    int phone;
+    int phone, number;
 
-    void setInformation(String n, String g, int ph) {
-        name = n;
-        gender = g;
-        phone = ph;
+
+    //constructor with parameters
+    Teacher(String cName, String cGender, int cPhone) {
+        name = cName;
+        gender = cGender;
+        phone = cPhone;
     }
+
+//    Just a method to compare with constructor
+    int multiplyValue(int v) {
+        return v * v;
+    }
+
+    //default constructor
+    Teacher() {
+        System.out.println("No value.");
+    }
+
+
+    // Initializing without a constructor
+//    void setInformation(String n, String g, int ph) {
+//        name = n;
+//        gender = g;
+//        phone = ph;
+//    }
 
     void showInformation() {
         System.out.println("Name: " + name);
-        System.out.println("Name: " + gender);
-        System.out.println("Name: " + phone);
+        System.out.println("Gender: " + gender);
+        System.out.println("Phone : " + phone);
 
     }
 }
--- src/oop/Test.java
+++ src/oop/Test.java
@@ -2,9 +2,16 @@ package oop;
 
 public class Test {
     public static void main(String[] args) {
-        Teacher teacher1 = new Teacher();
 
-        teacher1.setInformation("Naymur", "Male", 42345534);
+        Teacher teacher1 = new Teacher("Naymur", "Male", 42345534);
+        Teacher teacher2 = new Teacher();
+        //teacher1.setInformation("Naymur", "Male", 42345534); // usual way of passing data without constructor.
+
+        //using a constructor
+
+        teacher2.showInformation();
         teacher1.showInformation();
+
+        System.out.println("From line 15 -"+teacher1.multiplyValue(5));
     }
 }
