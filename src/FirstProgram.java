ef80f130bda8aafbd6057f6977d04d243219f452
Naymur Rahman <nrtusher1@gmail.com>
2024-12-17 08:53:49 +0600

    Changed nothing

--- src/FirstProgram.java
+++ src/FirstProgram.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;
+
 public class FirstProgram {
     public static void main(String[] args) {
-        System.out.print("Naymur Rahman Tushar!\n");
-        System.out.print("\"This is a quote to print along with the quotation.\"");
-        // \"\" used to print with the quote signs and this is a escape sequence.
+        Scanner Input = new Scanner(System.in);
+        int T = Input.nextInt();
+        while (T-- > 0) {
+            System.out.println("Ei Matha-Mota-Rogchota joddin achhe, non-profit diye kon ghaash ta katbo!");
+        }
+        Input.close();
     }
 }
